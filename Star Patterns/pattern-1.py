bbeb4e9e8662771edd85022292ad7ea61ac68ee3
TheNight-Owl <120689263+TheNight-Owl@users.noreply.github.com>
2023-01-24 22:10:57 +0530

    Create pattern-1.py

---
+++ Star Patterns/pattern-1.py
@@ -0,0 +1,13 @@
+# WAP to print the pattern given below
+# *
+# **
+# ***
+# ****
+# *****
+
+n=int(input("Enter the no. of rows:\n"))
+if n<=0:
+    print("I have got nothing to print")
+else:
+    for i in range(n+1):
+        print("*"*i)
